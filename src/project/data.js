aea8ab039503829090216a15dca4f4d77bb9426a
mynameisnastasya <nastasyabaklykova@gmail.com>
2026-09-18 05:22:01 +0700

    feat: add Variant D implementation design album

---
+++ src/project/data.js
@@ -0,0 +1,6 @@
+import core from './data/core.json';
+import systems from './data/systems.json';
+import procurement from './data/procurement.json';
+import qa from './data/qa.json';
+import album from './data/album.json';
+export const project={...core,...systems,...procurement,...qa,...album};
